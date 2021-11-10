08d96dc8c8d09c82f0f2d23fc9188b5f6dd47db8
murtada58 <murtada58@hotmail.com>
2021-11-10 18:33:21 +0000

    Made it possible to enter multiple numbers

--- index.js
+++ index.js
@@ -13,20 +13,29 @@ console.log("Welcome to the calculator!");
 
 console.log("Please enter an operator: ");
 const OPERATOR = readline.prompt();
-
-console.log("Please enter a number: ");
-const NUM1 = readline.prompt();
-
-console.log("Please enter another number: ");
-const NUM2 = readline.prompt();
-
-
-if (OPERATOR in OPERATORS)
-{
-    const RESULT = OPERATORS[OPERATOR](parseFloat(NUM1), parseFloat(NUM2));
-    console.log(`${NUM1} ${OPERATOR} ${NUM2} = ${RESULT}`);
-}
-else
+if (!(OPERATOR in OPERATORS))
 {
     console.log(`${OPERATOR} is not a valid operator`);
+    process.exit()
 }
+
+console.log(`How many numbers do you want to ${OPERATOR}? `);
+const NUMBERS_COUNT = readline.prompt();
+if (parseInt(NUMBERS_COUNT) < 2)
+{
+    console.log("You need to input at least 2 numbers");
+    process.exit()
+}
+
+console.log("Please enter number 1: ");
+let result = parseFloat(readline.prompt());
+let equation = `${result}`;
+for (let i = 2; i <= NUMBERS_COUNT; i++)
+{
+    console.log(`Please enter number ${i}: `);
+    const NUMBER = parseFloat(readline.prompt());
+    result = OPERATORS[OPERATOR](result, NUMBER);
+    equation += ` ${OPERATOR} ${NUMBER}`;
+}
+
+console.log(`${equation} = ${result}`);
