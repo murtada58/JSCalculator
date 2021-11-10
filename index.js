76370eec6b48b0928b8c06c776cd9299b2eae3cd
murtada58 <murtada58@hotmail.com>
2021-11-10 17:23:50 +0000

    Added basic arthmetic operators

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const readline = require("readline-sync");
 
 
+const OPERATORS = {
+                    "+" : (num1, num2) => num1  + num2,
+                    "-" : (num1, num2) => num1  - num2,
+                    "*" : (num1, num2) => num1  * num2,
+                    "/" : (num1, num2) => num1  / num2
+                };
+
+
 console.log("Welcome to the calculator!");
 
-console.log("Please enter a number");
-const num1 = readline.prompt();
+console.log("Please enter an operator: ");
+const OPERATOR = readline.prompt();
 
-console.log("Please enter another number");
-const num2 = readline.prompt();
+console.log("Please enter a number: ");
+const NUM1 = readline.prompt();
 
-const result = parseFloat(num1) * parseFloat(num2);
-console.log(`${num1} * ${num2} = ${result}`)
+console.log("Please enter another number: ");
+const NUM2 = readline.prompt();
+
+
+if (OPERATOR in OPERATORS)
+{
+    const result = OPERATORS[OPERATOR](parseFloat(NUM1), parseFloat(NUM2));
+    console.log(`${NUM1} ${OPERATOR} ${NUM2} = ${result}`);
+}
+else
+{
+    console.log(`${OPERATOR} is not a valid operator`);
+}
