e9537dd6c35b1676c5f777cadd2051fd6a01aa72
murtada58 <murtada58@hotmail.com>
2021-11-11 18:30:29 +0000

    Added input checks and made the code more DRY (also changed to use an array and reduce to compute the result)

--- index.js
+++ index.js
@@ -8,34 +8,49 @@ const OPERATORS = {
                     "/" : (num1, num2) => num1  / num2
                 };
 
-
+let running = true;
 console.log("Welcome to the calculator!");
-
-console.log("Please enter an operator: ");
-const OPERATOR = readline.prompt();
-if (!(OPERATOR in OPERATORS))
+while (running)
 {
-    console.log(`${OPERATOR} is not a valid operator`);
-    process.exit()
-}
+    console.log("Please enter an operator: ");
+    let  operator = readline.prompt();
+    while (!(operator in OPERATORS))
+    {
+        console.log(`${operator} is not a valid operator, please try again: `);
+        operator = readline.prompt();
+    }
+    
+    const NUMBERS_COUNT = Math.round(GetInputAsNumber(`How many numbers do you want to ${operator}? `));
+    let numbers = Array(NUMBERS_COUNT);
 
-console.log(`How many numbers do you want to ${OPERATOR}? `);
-const NUMBERS_COUNT = readline.prompt();
-if (parseInt(NUMBERS_COUNT) < 2)
+    if (NUMBERS_COUNT > 0)
+    {
+        let number = GetInputAsNumber("Please enter number 1: ");
+        numbers.push(number);
+        let equation = `${number}`;
+        for (let i = 2; i <= NUMBERS_COUNT; i++)
+        {
+            number = GetInputAsNumber(`Please enter number ${i}: `);
+            numbers.push(number);
+            equation += ` ${operator} ${number}`;
+        }
+        const RESULT = numbers.reduce((previousValue, currentValue) => OPERATORS[operator](previousValue, currentValue));
+        console.log(`${equation} = ${RESULT}`);
+    }
+
+    console.log("\n do another calculation? (y, n)");
+    running = readline.prompt().toLowerCase() === "y";
+}
+console.log("Bye :)");
+
+function GetInputAsNumber(message)
 {
-    console.log("You need to input at least 2 numbers");
-    process.exit()
+    console.log(message);
+    let response = parseFloat(readline.prompt());
+    while (isNaN(response))
+    {
+        console.log("That is not a valid number, please try again: ")
+        response = parseFloat(readline.prompt());
+    }
+    return response;
 }
-
-console.log("Please enter number 1: ");
-let result = parseFloat(readline.prompt());
-let equation = `${result}`;
-for (let i = 2; i <= NUMBERS_COUNT; i++)
-{
-    console.log(`Please enter number ${i}: `);
-    const NUMBER = parseFloat(readline.prompt());
-    result = OPERATORS[OPERATOR](result, NUMBER);
-    equation += ` ${OPERATOR} ${NUMBER}`;
-}
-
-console.log(`${equation} = ${result}`);
